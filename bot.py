2cb3e8fe436377eb107fb786d95d6204daaba07d
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-02-24 12:11:31 +0900

    Update bot.py
    -Добавил отправку логов в телеграм

--- bot.py
+++ bot.py
@@ -6,7 +6,17 @@ import time
 from environs import Env
 
 
-logger = logging.getLogger('bot')
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, chat_id, bot):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
 
 
 def send_notifications(bot, answer, chat_id):
@@ -30,7 +40,7 @@ def send_notifications(bot, answer, chat_id):
             )
 
 
-def make_requests(headers, bot, chat_id):
+def make_requests(headers, bot, chat_id, logger):
     url = 'https://dvmn.org/api/long_polling/'
     timestamp=time.time()
     while True:
@@ -39,8 +49,9 @@ def make_requests(headers, bot, chat_id):
                 url,
                 headers=headers,
                 params={'timestamp': timestamp},
-                timeout=90,
+                timeout=1,
             )
+            response.raise_for_status()
             answer = response.json()
             if ('status',  'found') in answer.items():
                 timestamp = answer.get('last_attempt_timestamp')
@@ -48,10 +59,10 @@ def make_requests(headers, bot, chat_id):
             else:
                 timestamp = answer.get('timestamp_to_request')
         except requests.exceptions.ReadTimeout:
-            logger.error('ReadTimeout: Нет проверенных работ!')
+            pass
         except requests.exceptions.ConnectionError:
+            logger.info('ConnectionError: Нет подключения к интернету!')
             time.sleep(90)
-            logger.error('ConnectionError: Нет подключения к интернету!')
 
 
 def main():
@@ -61,11 +72,14 @@ def main():
     dvm_api = env('DEVMAN_API')
     chat_id = env('TG_USER_CHAT_ID')
     bot = telegram.Bot(bot_api)
+    logger = logging.getLogger('bot')
+    logger.setLevel(logging.INFO)
     headers = {
         'Authorization': dvm_api,
     }
-    make_requests(headers, bot, chat_id)
-
+    logger.addHandler(TelegramLogsHandler(chat_id, bot))
+    logger.info('Бот запущен!')
+    make_requests(headers, bot, chat_id, logger)
 
 if __name__ == '__main__':
     main()
